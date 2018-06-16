4684bce7a59499660dd05bac3a0dc345064d2e53
Andy Gonzalez <aglez9538@gmail.com>
2018-06-16 08:11:55 -0400

    Completing BadVariableCapture example

--- CSharpLanguageDemo/ClosureExampleClass.cs
+++ CSharpLanguageDemo/ClosureExampleClass.cs
@@ -8,5 +8,18 @@ namespace CSharpLanguageDemo
 {
     class ClosureExampleClass
     {
+        public static void BadVariableCapture()
+        {
+            var actions = new Action[10];              // One instance for the loop's variable
+            for (int x = 0; x < actions.Length; x++)
+            {
+                int y = x;                             // Multiple instances of variable y
+                actions[x] = () =>
+                {
+                    int z = x;                         // Local Variable
+                    Console.WriteLine($"{x} {y} {z}"); // Capturing outer variables
+                };
+            }
+        }
     }
 }
